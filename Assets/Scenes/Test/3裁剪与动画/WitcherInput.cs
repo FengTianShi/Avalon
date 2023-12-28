3c337f4ca95b16b328329ed9a59969565acbeef0
FengTianShi <fts2470216@gmail.com>
2023-12-28 22:11:46 +0900

    Update WitcherInput.cs

--- Assets/Scenes/Test/3裁剪与动画/WitcherInput.cs
+++ Assets/Scenes/Test/3裁剪与动画/WitcherInput.cs
@@ -8,30 +8,85 @@ public class WitcherInput : MonoBehaviour
 
     private Animator anim;
 
+    private CapsuleCollider2D capsuleCollider;
+
     [SerializeField]
     private float moveSpeed = 10;
 
     [SerializeField]
-    private float jumpForce = 10;
+    private float jumpForce = 5;
+
+    [SerializeField]
+    private float xInput;
+
+    [SerializeField]
+    private int facing = 1;
+
+    [SerializeField]
+    private bool isMoving;
+
+    [SerializeField]
+    private bool isGrounded;
+
+    [SerializeField]
+    private LayerMask groundLayer;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
         anim = GetComponent<Animator>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+        groundLayer = LayerMask.GetMask("Ground");
     }
 
     void Update()
     {
-        float xInput = Input.GetAxisRaw("Horizontal");
+        xInput = Input.GetAxisRaw("Horizontal");
 
-        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+        UpdateFacing();
 
-        if (Input.GetKeyDown(KeyCode.Space) && rb.velocity.y == 0)
+        CheckIsGrounded();
+
+        if (isGrounded)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            Run();
+            Jump();
         }
 
-        anim.SetBool("isMoving", rb.velocity.x != 0);
+        UpdateAnimator();
+    }
+
+    private void Run()
+    {
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+        isMoving = rb.velocity.x != 0;
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isGrounded = false;
+        }
+    }
+
+    private void UpdateFacing()
+    {
+        if (xInput != 0)
+        {
+            facing = (int)xInput;
+            transform.localScale = new Vector3(facing, 1, 1);
+        }
+    }
+
+    private void CheckIsGrounded()
+    {
+        isGrounded = Physics2D.OverlapCapsule(capsuleCollider.bounds.center, capsuleCollider.size, CapsuleDirection2D.Vertical, 0, groundLayer);
+    }
+
+    private void UpdateAnimator()
+    {
+        anim.SetBool("isMoving", isMoving);
     }
 }
